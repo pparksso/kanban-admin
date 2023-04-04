d7f3a7547471c476af453c2c3ed0f4a75da658c5
Park Sohee <pssh5124@gmail.com>
2023-04-04 11:25:10 +0900

    chore: nuxt.config 수정(picocss cdn으로 수정)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@pinia/nuxt', '@picocss/pico'],
+    modules: ['@pinia/nuxt'],
     app: {
         head: {
             charset: 'utf-8',
             title: 'kanbanList-admin',
+            link: [
+                {
+                    href: 'https://unpkg.com/@picocss/pico@1.*/css/pico.min.css',
+                    rel: 'stylesheet',
+                },
+            ],
             htmlAttrs: {
                 lang: 'ko',
             },
